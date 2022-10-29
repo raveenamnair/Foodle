bc18fa7e98ff6b3e0418ada28fa9c6ea35e23f53
raveenamnair <nair64@purdue.edu>
2022-10-29 14:55:29 -0400

    adding backend stuff

---
+++ backend/myApi/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
+
+app.route('/user')
+  .get(function(req, res, next) {
+    connection.query(
+      "SELECT * FROM `user`", 
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
+
+app.route('/user/:username')
+.get(function(req, res, next) {
+connection.query(
+    "SELECT * FROM `user` WHERE username = ?", req.params.username, 
+    function(error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+    }
+);
+});
+
+app.get('/status', (req, res) => res.send('Working!'));
+
+// Port 8080 for Google App Engine
+app.set('port', process.env.PORT || 3000);
+app.listen(3000);
