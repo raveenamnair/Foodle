bf3b06c4d0ef108f19b6d4636c5d55eecb3422f1
ChristineZh0u <zhou977@purdue.edu>
2022-12-05 21:15:44 -0500

    blabala

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,11 @@ function App() {
     <div>
         <h1>Foodle</h1>
           <nav>
-              <Link to="/">Home</Link><br/>
-              <Link to="/exploreRecipes">Explore Recipes</Link><br/>
+              <Link to="/">Home</Link><span/>
+              <Link to="/exploreRecipes">Explore Recipes</Link><span/>
               {sessionStorage.getItem('username') && <Link to="/profile">Profile</Link>}
-              {!sessionStorage.getItem('username') && <Link to="/login">Login</Link>} <br/>
-              <Link to="/reports">Reports</Link><br />
+              {!sessionStorage.getItem('username') && <Link to="/login">Login</Link>} <span/>
+              <Link to="/reports">Reports</Link><span />
           </nav>
       </div>
   );
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {decode as base64_decode, encode as base64_encode} from 'base-64';
